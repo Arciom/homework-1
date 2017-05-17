67efe28db36f2191ea5809f92d32c420a46774e4
Arciom <swert.rem@gmail.com>
2017-05-17 21:55:59 +0300

    Домашнее задание 1-3.

--- sandbox/src/main/java/by/Homework2/Coordinates.java
+++ sandbox/src/main/java/by/Homework2/Coordinates.java
@@ -2,8 +2,8 @@ package by.Homework2;
 
 public class Coordinates {
   public static void main(String[] args) {
-    Point p1 = new Point(2.8, 6.9);
-    Point p2 = new Point(6.9, 8.1);
+    Point p1 = new Point(0.0, 0.0);
+    Point p2 = new Point(0.0, 0.0);
 
     System.out.println("Растояние между точками: " + p1.distance(p2));
   }
---
+++ sandbox/src/test/java/by/Homework2/PointTest.java
@@ -0,0 +1,31 @@
+package by.Homework2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by arciom on 17.05.2017.
+ */
+public class PointTest {
+  public PointTest() {
+
+  }
+  @Test
+  public void rightResultTestDistance() {
+    Point p1 = new Point(2.8, 6.9);
+    Point p2 = new Point(6.9, 8.1);
+    Assert.assertEquals(p1.distance(p2), 4.272001872658766);
+  }
+  @Test
+  public void wrongResultTestDistance() {
+    Point p1 = new Point(2.8, 6.9);
+    Point p2 = new Point(6.9, 8.1);
+    Assert.assertEquals(p1.distance(p2), 6.272001872658766);
+  }
+  @Test
+  public void nullResultTestDistance() {
+    Point p1 = new Point(0.0, 0.0);
+    Point p2 = new Point(0.0, 0.0);
+    Assert.assertEquals(p1.distance(p2), 0.0);
+  }
+}
