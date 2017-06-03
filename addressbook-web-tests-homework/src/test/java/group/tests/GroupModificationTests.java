5c2761d22401e0beda646d8f049c6630272ff0b8
Arciom <swert.rem@gmail.com>
2017-06-03 23:17:25 +0300

    Лекция 4.5. Формирование коллекции

--- addressbook-web-tests-homework/src/test/java/group/tests/GroupModificationTests.java
+++ addressbook-web-tests-homework/src/test/java/group/tests/GroupModificationTests.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import testBase.TestBase;
 
+import java.util.List;
+
 /**
  * Created by arciom on 24.05.2017.
  */
@@ -12,18 +14,18 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1",
               null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test1", "test1"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before );
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() );
   }
 
 }
