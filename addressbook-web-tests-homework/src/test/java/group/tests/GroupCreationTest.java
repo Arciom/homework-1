cf59c1bfef8291c8081ccc342117c3633e0087ea
Arciom <swert.rem@gmail.com>
2017-06-29 08:01:37 +0300

    Лекция 6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests-homework/src/test/java/group/tests/GroupCreationTest.java
+++ addressbook-web-tests-homework/src/test/java/group/tests/GroupCreationTest.java
@@ -2,19 +2,37 @@ package group.tests;
 
 import moduleGroup.GroupData;
 import moduleGroup.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import testBase.TestBase;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while(line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new  GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
@@ -23,7 +41,7 @@ public class GroupCreationTest extends TestBase {
 
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
