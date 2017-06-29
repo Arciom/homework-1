8bec8de57a72ad3e9314fd54b495c3938bae22c0
Arciom <swert.rem@gmail.com>
2017-06-29 23:11:58 +0300

    Лекция 6.7. Формат JSON

--- addressbook-web-tests-homework/src/test/java/group/tests/GroupCreationTest.java
+++ addressbook-web-tests-homework/src/test/java/group/tests/GroupCreationTest.java
@@ -1,5 +1,7 @@
 package group.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import moduleGroup.GroupData;
 import moduleGroup.Groups;
@@ -20,7 +22,22 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() throws IOException {
+  public Iterator<Object[]> validGroupsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader
+            (new File("src/test/resources/groups.json")));
+    String json = "";
+    String line = reader.readLine();
+    while(line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());//= List<Groupata>.class
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @DataProvider
+  public Iterator<Object[]> validGroupsFromXml() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
     String xml = "";
     String line = reader.readLine();
@@ -34,7 +51,7 @@ public class GroupCreationTest extends TestBase {
     return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test(dataProvider = "validGroups")
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
